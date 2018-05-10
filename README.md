1769303650e14696f03a4a0a48958481ebcb1a38
semorenore <semorenore@unal.edu.co>
2018-05-09 23:36:23 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-## webapp_phase1
+###  1. Name of application: *Nice Housing New York*
 
-undefined
+### 2. Key words: 
+Simple, easy, practical.
+
+### 3. Dataset description:
+NY District geoshapes, [link](https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson), JSON, coordinates, all data.
+
+### 4. Brief description:
+Tihs project will make the search of a place to live for a NYU student (stern school of Business primarily) easier, faster and a lot better in comparisson to search in a lot of sites and being unable to compare all of them at once.
+
+#### - Map View:
+[Y] The map is centered in the NYU stern school of Business.
+[Y] The map has a delimiter between districts of New York city.
+
+#### - Data Visulization:
+[N]
+[N]
+
+#### - Interaction Form:
+[N]
+[Y] Marker for NYU stern school of Business.
+[N]
+[Y] Button to delimit the New York city districts, Buttron to place or hide a marker in the NYU stern school of Business.
+### 6. Test Case: 
+Google Chrome, Internet Explorer and Microsoft Edge.
--- index.js
+++ index.js
@@ -1,6 +1,90 @@
+const API_KEY = "AIzaSyAmsoNWToucbKC9uArndPzSpqS15bITqTM";
+var count2=1;
+var count=0;
+var ver;
+var map;
+var ny_coordinates = {lat:40.703092,lng:-73.989741};
+var u_coordinates = {lat:40.7291,lng:-73.9965};
+var u_marker;
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
+function coloresRandom(){
+  var color = ["#023fa5", "#7d87b9", "#bec1d4", "#d6bcc0", "#bb7784", "#8e063b", "#4a6fe3", "#8595e1", "#b5bbe3", "#e6afb9", "#e07b91", "#d33f6a", "#11c638", "#8dd593", "#c6dec7", "#ead3c6", "#f0b98d", "#ef9708", "#0fcfc0"];
+  return color[Math.floor(Math.random()*color.length)];
+}
+function uMarker(){
+  if (ver!=0) {
+    u_marker = new google.maps.Marker({
+      position: u_coordinates,
+      map: map
     });
-})();
+
+  }
+  else if (ver==0) {
+      u_marker.setMap(null);
+
+  }
+    count2=count2+1;
+    ver=count2%2;
+
+}
+
+function districts() {
+  if (count==0) {
+    map.data.loadGeoJson('https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson');
+    map.data.setStyle(function(feature) {
+      var color = coloresRandom();
+      return {
+        fillColor: color,
+        strokeWeight: 1
+      };
+    });
+  }
+  count=+1;
+}
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10.7,
+    center: u_coordinates
+  });
+
+}
+
+function markerEvents(marker) {
+  if(marker!="undefined"){
+    marker.addListener("click",function () {
+      getRoute();
+    });
+  }
+}
+
+function getRoute() {
+  var request = {
+    origin: ny_marker.position,
+    destination: bro_marker.position,
+    travelMode: 'DRIVING'
+  }
+  directionsRenderer.setMap(map);
+  directionsService.route(request, function(result,status){
+    if(status=="OK"){
+      directionsRenderer.setDirections(result);
+    }
+  })
+}
+
+$("document").ready(function(){
+  $("#showU").on("click",uMarker)
+  $("#showDistricts").on("click",districts)
+});
+
+/*function drawPolygon(polygon,color) {
+  polygon = new google.maps.Polygon({
+          paths: triangleCoords,
+          strokeColor: color,
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: color,
+          fillOpacity: 0.35
+});
+  polygon.setMap(map);
+}*/
